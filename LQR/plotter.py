cb3468a9a9f5686a02cfd8a8f8f6029e0af3e882
Sebastijan Veselic <sebastijan.veselic@gmail.com>
2018-07-11 14:47:08 +0100

    almost done.
    mostly needs tidying up and more meta-information

--- LQR/plotter.py
+++ LQR/plotter.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python
-
 import collections
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import time
 from matplotlib.lines import Line2D
 import matplotlib.animation as animation
 
-# make this a bit more serious
-def prepare_data():
-    with open("Trajectory.txt") as f:
+# We will need these two functions multiple times
+def prepare_data(file):
+    with open(file) as f:
         data = f.read()
     data = data.split('\n')
     return data
-
-data = prepare_data()
 
 def split_data(data, it, type):
     l = []
@@ -33,20 +29,52 @@ def split_data(data, it, type):
             l[i] = float(l[i])
     return l
 
+################################
+####### Prepare the data #######
+################################
+
+data = prepare_data('Trajectory.txt')
+x_hist = prepare_data('StateHistory.txt')
+nn_hist = prepare_data('UserHistory.txt')
+u_hist_star = prepare_data('OptimalInputHistory.txt')
+u_hist = prepare_data('InputHistory.txt')
+
 fl = 'float'
 st = 'str'
-intt = 'int'
+nr = 'int'
 
+################################
+##### Define all variables #####
+################################
+
+# Trajectory variables
 x = split_data(data, 0, fl)
 y = split_data(data, 1, fl)
 theta = split_data(data, 2, fl)
 xDot = split_data(data, 3, fl)
 yDot = split_data(data, 4, fl)
-wayp_num = split_data(data, 5, intt)
+wayp_num = split_data(data, 5, nr)
 traj_names = split_data(data, 6, st)
 traj_names = list(set(traj_names))
+
 # Final individual trajectory names
 traj_names.sort()
+
+# State history variables
+x_hist_x = split_data(x_hist, 0, fl)
+x_hist_y = split_data(x_hist, 1, fl)
+x_hist_theta = split_data(x_hist, 2, fl)
+
+# Nearest neighbour trajectory history
+nn_hist = split_data(nn_hist, 0, st)
+
+# User input history
+u_hist_xDot = split_data(u_hist, 0, fl)
+u_hist_yDot = split_data(u_hist, 1, fl)
+
+# Optimal input history
+u_hist_xDot_star = split_data(u_hist_star, 0, fl)
+u_hist_yDot_star = split_data(u_hist_star, 1, fl)
 
 traj_info = collections.defaultdict(list)
 traj_identifiers = []
@@ -67,16 +95,24 @@ for traj in traj_names:
         counter += 1
     counter = 0
 
+
+################################
+## Start plotting everything ###
+################################
+
 fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1)
-ax2 = fig.add_subplot(2, 2, 3)
-ax3 = fig.add_subplot(2, 2, 4)
+ax1 = fig.add_subplot(3, 1, 1)
+ax2 = fig.add_subplot(3, 2, 3)
+ax3 = fig.add_subplot(3, 2, 4)
+ax4 = fig.add_subplot(3, 2, 5)
+ax5 = fig.add_subplot(3, 2, 6)
+
 
 traj_coords = [()]
 traj_co = 0
 co = 0
 
-plt.subplot(211)
+plt.subplot(311)
 for traj in traj_names:
     for st in traj_identifiers:
         if traj == st[0]:
@@ -92,27 +128,86 @@ for traj in traj_names:
 
 complete_traj = ()
 
-# we want the trajectories to be pre-plotted - done
-# we want the waypoint indicator - done
-# we want the system state information
-# we want user input
-# we want optimal input
+font = {'family': 'arial',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 11,
+        }
+
+u_x, u_y, x_x, x_y, u_opt_x, u_opt_y = [], [], [], [], [], []
 
 
-global currx, curry
-currx, curry = [], []
+def filter(input_list, frame, el):
+    return input_list[frame:frame+1][el]
 
 def update(frame):
-    print(frame)
-    global currx, curry
-    plt.subplot(211) # x coords will be replaced by the system information
-    line, = plt.plot(x_coords[frame:frame+1], y_coords[frame:frame+1], marker='o', color="Black")
-    plt.subplot(212)
-    currx.append(x_coords[frame:frame+1][0])
-    curry.append(y_coords[frame:frame+1][0])
-    line2, = plt.plot(currx, curry, marker='o', color="#7EC0EE")
-    line2.axes.axis([0, 1000, 0, 250])
-    return line, line2
+    # This is the trajectory history
+    curr_wayp = filter(nn_hist, frame, 0)
+    wayp_x = traj_info[curr_wayp][0]
+    wayp_y = traj_info[curr_wayp][1]
+
+    # This is the user input history
+    user_x = filter(u_hist_xDot, frame, 0)
+    user_y = filter(u_hist_yDot, frame, 0)
+    u_x.append(user_x)
+    u_y.append(user_y)
+
+    # this is the optimal input history
+    u_star_x = filter(u_hist_xDot_star, frame, 0)
+    u_star_y = filter(u_hist_yDot_star, frame, 0)
+    u_opt_x.append(u_star_x)
+    u_opt_y.append(u_star_y)
+
+    # State history
+    state_x = filter(x_hist_x, frame, 0)
+    state_y = filter(x_hist_y, frame, 0)
+    x_x.append(state_x)
+    x_y.append(state_y)
+
+    plt.subplot(311)
+    # this plots the current waypoint
+    plt.title('Potential trajectories with unfolding current state (red) and nearest waypoint (black)', fontdict=font)
+    line, = plt.plot(wayp_x, wayp_y, marker='o', color="Black")
+
+    # this plots the unfolding state
+    line2, = plt.plot(x_x, x_y, marker='o', color="Red")
+
+    plt.subplot(323)
+    plt.title('User input', fontdict=font)
+
+    # this plots the user input
+    line3, = plt.plot(u_x, u_y, marker='o', color="#7EC0EE")
+    line3.axes.axis([-1, 1, -1, 1])
+    u_x.pop()
+    u_y.pop()
+
+    plt.subplot(324)
+    plt.title('Optimal input', fontdict=font)
+    # this plots the optimal input info
+    line4, = plt.plot(u_opt_x, u_opt_y, marker='o', color="Green")
+    line4.axes.axis([min(u_opt_x), max(u_opt_x), min(u_opt_y), max(u_opt_y)])
+
+    # finally, we should have an obtained and optimal state value comparison.
+    plt.subplot(325)
+    plt.title('Unfolding (red) and predicted (gray) state', fontdict=font)
+
+    # unfolding state
+    line5, = plt.plot(x_x, x_y, marker='o', color="Red")
+    # unfolding state prediction
+    line6, = plt.plot(u_opt_x, u_opt_y, marker='o', color="Gray")
+
+    plt.subplot(326)
+    ax5.axis('off')
+    time_text = plt.text(.05, .8, '', fontsize=10)
+
+    plt.title('Meta-information', fontdict=font)
+    time_text.set_text("Elapsed waypoint: " + str(wayp_num[frame]))
+
+
+    return line, line2, line3, line4, line5, line6, time_text
+
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.5,
+                    wspace=0.35)
 
 animation = animation.FuncAnimation(fig, update, interval=100, blit=True)
 
